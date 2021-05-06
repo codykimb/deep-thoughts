67c890361a07e6a9a3a74b3ca7fe86c499aa8abb
Cody Kimb <codykimbell@gmail.com>
2021-05-05 19:42:01 -0500

    implement auth middleware to populate me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
+          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+          },
         // get all thoughts
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
@@ -33,8 +45,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
           
-            return user;
+            return { token, user };
           },
           login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
@@ -49,7 +62,8 @@ const resolvers = {
               throw new AuthenticationError('Incorrect credentials');
             }
           
-            return user;
+            const token = signToken(user);
+            return { token, user };
           }
       }
   };
